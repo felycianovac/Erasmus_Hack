4ac14c3787af3c794da7f1821c33bb875c6c20ca
qopas <grigoras.dumiitru@gmail.com>
2024-05-19 05:08:39 +0300

    Implement all huiovie api + 2.

--- src/main/java/org/example/Specialization/SpecializationController.java
+++ src/main/java/org/example/Specialization/SpecializationController.java
@@ -17,7 +17,7 @@ public class SpecializationController {
         this.specializationService = specializationService;
     }
 
-    @GetMapping("/{university_id}")
+    @GetMapping("/univ_id:{university_id}")
     public ResponseEntity<List<SpecializationDTO>> getSpecializationsByUniversityId(@PathVariable("university_id") Integer universityId) {
         List<SpecializationDTO> specializationDTOs = specializationService.getSpecializationsByUniversityId(universityId);
         return ResponseEntity.ok(specializationDTOs);
